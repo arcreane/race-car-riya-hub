248bca3c85d443b8bcabe3180c5fef468ba168a8
riya <lamariya207@gmail.com>
2020-04-07 15:47:01 +0200

    This is the beginning of the project

---
+++ Race_against_time/Car_Parts/Parts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Race_against_time
+{
+    public enum CAR_PARTS
+    {
+        ENGINE,
+        RIGHT_FRONT,
+        LEFT_FRONT,
+        RIGHT_REAR,
+        LEFT_REAR,
+        STEERING_WHEEL,
+        BRAKES
+    }
+
+    public class Parts
+    {
+    }
+}
