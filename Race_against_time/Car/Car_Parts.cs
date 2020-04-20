b730e737d66c22dfe41896056c805738b111c228
riya-hub <56731444+riya-hub@users.noreply.github.com>
2020-04-20 23:57:28 +0200

    Add files via upload
    This is my final project

---
+++ Race_against_time/Car/Car_Parts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_race
+{
+    public enum CAR_PARTS
+    {
+        ENGINE,
+        RIGHT_FRONT,
+        LEFT_FRONT,
+        RIGHT_REAR,
+        LEFT_REAR,
+        STEERING_WHEEL,
+        BRAKES
+    }
+
+    public class Parts
+    {
+        public CAR_PARTS m_carparts;
+        
+        internal void ApplyEffect(Player p_player)
+        {
+            switch (m_carparts)
+            {
+                case CAR_PARTS.ENGINE:
+                    break;
+                case CAR_PARTS.RIGHT_FRONT:
+                    break;
+                case CAR_PARTS.LEFT_FRONT:
+                    break;
+                case CAR_PARTS.RIGHT_REAR:
+                    break;
+                case CAR_PARTS.LEFT_REAR:
+                    break;
+                case CAR_PARTS.STEERING_WHEEL:
+                    break;
+                case CAR_PARTS.BRAKES:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
